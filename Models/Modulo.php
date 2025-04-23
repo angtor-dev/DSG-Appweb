ad1a30c02272c17ed4a756d24edfb5a459a2e8e1
Angel Torres <angtor.dev@gmail.com>
2025-04-23 00:34:59 -0400

    Refac. Se añaden constantes para los diferentes módulos en la clase Modulo

--- Models/Modulo.php
+++ Models/Modulo.php
@@ -5,6 +5,19 @@ class Modulo extends Model
 {
     private string $nombre;
 
+    public const BITACORA = 'bitacora';
+    public const ROLES = 'roles';
+    public const USUARIOS = 'usuarios';
+    public const AREAS = 'areas';
+    public const ASISTENCIAS = 'asistencias';
+    public const CATEGORIAS = 'categorias';
+    public const DEPARTAMENTOS = 'departamentos';
+    public const INVENTARIO = 'inventario';
+    public const MEDIDAS = 'medidas';
+    public const NOTIFICACIONES = 'notificaciones';
+    public const TAREAS = 'tareas';
+    public const TRABAJADORES = 'trabajadores';
+
     // Override para impedir eliminar
     public function eliminar(bool $eliminadoLogico = true) : bool
     {
