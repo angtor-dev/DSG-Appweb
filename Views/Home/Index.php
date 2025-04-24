45075b6bb242a324580f8c9078addddfd2c9cadf
Angel Torres <angtor.dev@gmail.com>
2025-04-23 22:27:58 -0400

    Feat. Pantalla de ejemplo de dashboard

--- Views/Home/Index.php
+++ Views/Home/Index.php
@@ -1,1 +1,181 @@
-<h1>Home / Index</h1>
+<div class="p-3">
+    <!-- Titulo -->
+     <h3>Dashboard</h3>
+    <!-- Cartas superiores -->
+    <div class="row justify-content-center g-3">
+        <div class="col">
+            <div class="card border">
+                <div class="card-body justify-content-center align-items-center d-flex flex-column">
+                    <h4 class="card-title">4</h4>
+                    <p class="card-text">Tareas activas</p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card border">
+                <div class="card-body justify-content-center align-items-center d-flex flex-column">
+                    <h4 class="card-title">1</h4>
+                    <p class="card-text">Tareas sin asignar</p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card border">
+                <div class="card-body justify-content-center align-items-center d-flex flex-column">
+                    <h4 class="card-title">54</h4>
+                    <p class="card-text">Dato 3</p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card border">
+                <div class="card-body justify-content-center align-items-center d-flex flex-column">
+                    <h4 class="card-title">2</h4>
+                    <p class="card-text">Dato 4</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Tablas inferiores -->
+     <div class="row justify-content-center g-3 mt-0">
+        <div class="col">
+            <div class="card border">
+                <div class="card-header border-primary" style="border-width: 2px;">
+                    <i class="fa-solid fa-table"></i>
+                    <b>Tareas de hoy</b>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-sm table-striped table-bordered mb-0">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Nombre</th>
+                                    <th scope="col">Depart.</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Podar cesped</td>
+                                    <td>Jardineria</td>
+                                </tr>
+                                <tr>
+                                    <td>Reparación de ventana</td>
+                                    <td>Herreria</td>
+                                </tr>
+                                <tr>
+                                    <td>Instalación de tuberia</td>
+                                    <td>Plomeria</td>
+                                </tr>
+                                <tr>
+                                    <td>Podar cesped</td>
+                                    <td>Jardineria</td>
+                                </tr>
+                                <tr>
+                                    <td>Reparación de ventana</td>
+                                    <td>Herreria</td>
+                                </tr>
+                                <tr>
+                                    <td>Instalación de tuberia</td>
+                                    <td>Plomeria</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card border">
+                <div class="card-header border-primary" style="border-width: 2px;">
+                    <i class="fa-solid fa-toolbox"></i>
+                    <b>Entradas recientes</b>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-sm table-striped table-bordered mb-0">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Articulo</th>
+                                    <th scope="col">Cant.</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Pintura negra</td>
+                                    <td>2 Lts</td>
+                                </tr>
+                                <tr>
+                                    <td>Escalera de 6 peldaños</td>
+                                    <td>1 ud</td>
+                                </tr>
+                                <tr>
+                                    <td>Cinta Métrica Acero 5m x 19mm</td>
+                                    <td>2 uds</td>
+                                </tr>
+                                <tr>
+                                    <td>Destornillador Plano 6mm x 100mm</td>
+                                    <td>1 ud</td>
+                                </tr>
+                                <tr>
+                                    <td>Clavos de Acero con Cabeza 2 1/2"</td>
+                                    <td>10 Kgs</td>
+                                </tr>
+                                <tr>
+                                    <td>Aceite para Motor de 4 Tiempos</td>
+                                    <td>5 Lts</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card border">
+                <div class="card-header border-primary" style="border-width: 2px;">
+                    <i class="fa-solid fa-user"></i>
+                    <b>Tabla resumen 3</b>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-sm table-striped table-bordered mb-0">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Ejemplo Nr.3</th>
+                                    <th scope="col">Fila 2</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Entrada Uno</td>
+                                    <td>1</td>
+                                </tr>
+                                <tr>
+                                    <td>Segunda entrada tabla</td>
+                                    <td>2</td>
+                                </tr>
+                                <tr>
+                                    <td>Otra entrada</td>
+                                    <td>3</td>
+                                </tr>
+                                <tr>
+                                    <td>Ejemplo de columna de tabla</td>
+                                    <td>4</td>
+                                </tr>
+                                <tr>
+                                    <td>Ejemplo de columna de tabla</td>
+                                    <td>5</td>
+                                </tr>
+                                <tr>
+                                    <td>Ejemplo de columna de tabla</td>
+                                    <td>6</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+     </div>
+     
+</div>
