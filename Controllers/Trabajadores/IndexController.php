0ca081386823cf9709730b8fbe6e2de62f7ece21
Angel Torres <angtor.dev@gmail.com>
2025-05-05 21:05:32 -0400

    Fix. Requiere enums

--- Controllers/Trabajadores/IndexController.php
+++ Controllers/Trabajadores/IndexController.php
@@ -1,6 +1,8 @@
 <?php
 requiereAutenticacion();
 requierePermiso(Modulo::TRABAJADORES, Permiso::CONSULTAR);
+require_once("Models/Enums/Turno.php");
+require_once("Models/Enums/Cargo.php");
 
 $trabajadores = Trabajador::listar();
 
