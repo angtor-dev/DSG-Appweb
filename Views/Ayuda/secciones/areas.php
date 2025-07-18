bf556b8c55e1661a6e5287c487251d5863677935
Angel Torres <angel@snap360app.com>
2025-07-18 09:38:21 -0400

    avances

--- Views/Ayuda/secciones/areas.php
+++ Views/Ayuda/secciones/areas.php
@@ -8,4 +8,30 @@
             y eliminar aquellas que ya no son necesarias.
         </p>
     </div>
+    <div class="ayuda-sub-header" id="objetivos-clave">Registrar Área</div>
+    <div class="ayuda-texto">
+        <p>
+            Los pasos para registrar un área son los siguientes:
+        </p>
+        <ol>
+            <li>
+                Ingrese al módulo de Áreas desde el menú lateral.
+                <div class="img">
+                    <img src="public/img/ayuda/img04.png">
+                    <div>Modulo de Areas resaltado</div>
+                </div>
+            </li>
+            <li>
+                Haga clic en el botón "Registrar Área".
+            </li>
+            <li>
+                Complete los campos requeridos, como el nombre del área y una breve
+                descripción.
+            </li>
+            <li>
+                Presione el botón "Guardar" para registrar el área.
+            </li>
+        </ol>
+    </div>
+    
 </section>
