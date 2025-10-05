c219e843a0985a5506337143c28e53cda89f044f
Angel Torres <angel@snap360app.com>
2025-10-05 18:51:06 -0400

    Feat. Se agregan constantes de correo y ejemplo de archivo user_config.php

---
+++ user_config.example.php
@@ -0,0 +1,25 @@
+<?php
+// En este archivo pueden sobrescribir las constantes definidas en config.php
+// Deben borrar la extension .example para que funcione (es decir, debe quedar user_config.php)
+const LOCAL_DIR = "/DSG-Appweb";
+const DEVELOPER_MODE = false;
+
+const DB_HOST = "";
+const DB_NAME = "";
+const DB_USER = "";
+const DB_PASSWORD = "";
+
+const DB_USERS_HOST = "";
+const DB_USERS_NAME = "";
+const DB_USERS_USER = "";
+const DB_USERS_PASSWORD = "";
+const DB_USERS_PORT = "";
+
+const DEP_NAME = "";
+const DEP_NAME_M = "";
+const DEP_NAME_S = "";
+
+const SYS_EMAIL = "";
+const SYS_EMAIL_PASS = "";
+const SYS_EMAIL_NAME = "";
+const SYS_EMAIL_HOST = "";
